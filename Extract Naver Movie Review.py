ab136b59ef025288578a19438336261f89f7c4ad
SeonIkPark <parksimis@gmail.com>
2020-07-25 17:27:09 +0900

    2020-07-25 Extract Naver Movie Review still ~ing

--- Extract Naver Movie Review.py
+++ Extract Naver Movie Review.py
@@ -6,33 +6,50 @@ from bs4 import BeautifulSoup
 driver_path = 'resources/chromedriver'
 url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200721'
 
-'''browser = webdriver.Chrome(executable_path=driver_path) # Chrome driver
+browser = webdriver.Chrome(executable_path=driver_path) # Chrome driver
 browser.get(url)
 
 soup = BeautifulSoup(browser.page_source, 'html.parser')
 
 # 첫 화면에서 50위까지의 영화 상세정보 사이트를 가져옴
 ranking_links = soup.find_all('div', {"class" : "tit5"}, {'a' : 'href'})
+
 import time
-# time.sleep(1)
+
+naver_reviews = []
 
 for movie_link in ranking_links:
     movie_url = movie_link.a['href']
     browser.get('https://movie.naver.com/' + movie_url)
-    button = browser.find_element_by_css_selector('.end_sub_tab li:nth-child(5) a').click()
     new_url = browser.page_source
-    soup = BeautifulSoup(new_url, 'html.parser')'''
+    soup = BeautifulSoup(new_url, 'html.parser')
+    #button = browser.find_element_by_css_selector('.end_sub_tab li:nth-last-child(3) a').click()
+    button = browser.find_element_by_css_selector('.end_sub_tab li').get_attribute('title').click()
+    #button[title='평점'].a['href'].click()
+    time.sleep(1)
+    # new_url = browser.page_source
+    # soup = BeautifulSoup(new_url, 'html.parser')
 
+    # 각 영화의 리뷰 페이지의 첫 링크를 가져옴
+    review_link = browser.find_element_by_css_selector('.ifr_module2 iframe').get_attribute('src')
+    browser.get(review_link) # 링크 접속
+    time.sleep(1)
+    # 각 영화의 20페이지까지 가져옴
+    for i in range(1, 3):
+        new_url = review_link + "&page=" + str(i)
+        browser.get(new_url)
 
-driver_path = 'resources/chromedriver'
-url = 'https://movie.naver.com/movie/bi/mi/point.nhn?code=171539'
+        # 영화가 담겨있는 box 가져옴
+        boxes = browser.find_elements_by_css_selector('.score_result li')
 
-browser = webdriver.Chrome(executable_path=driver_path) # Chrome driver
-browser.get(url)
-temp_link = browser.page_source
-soup = BeautifulSoup(temp_link, 'html.parser')
-temp = soup.find_all({'class' : "ifr_area basic_ifr"}, {'class' : 'score_result'})
-#temp = browser.find_elements_by_css_selector('.ifr_area.basic_ifr .score_result .score_reple')
+        for i in range(len(boxes)):
+            a = boxes[i].find_element_by_css_selector('.star_score').text
+            b = boxes[i].find_element_by_css_selector('#_filtered_ment_'+str(i)).text
+            c = boxes[i].find_element_by_css_selector('.score_reple dt em:nth-child(2)').text
+            naver_reviews.append([a, b, c])
+            #print(boxes[i].find_element_by_css_selector('.star_score').text)
+            #print(boxes[i].find_element_by_css_selector('#_filtered_ment_'+str(i)).text)
+            #print(boxes[i].find_element_by_css_selector('.score_reple dt em:nth-child(2)').text)
 
 browser.quit()
 
